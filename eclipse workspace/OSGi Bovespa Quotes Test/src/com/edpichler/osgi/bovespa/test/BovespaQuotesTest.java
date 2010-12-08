51bbb07e32bb2359ec3076ea83bc2f5dafd8ed1e
pichler <pichler@192.168.0.66>
2010-12-08 21:18:49 -0200

    Alterado assinatura do método para ser a Interface e não mais a implementação.

--- eclipse workspace/OSGi Bovespa Quotes Test/src/com/edpichler/osgi/bovespa/test/BovespaQuotesTest.java
+++ eclipse workspace/OSGi Bovespa Quotes Test/src/com/edpichler/osgi/bovespa/test/BovespaQuotesTest.java
@@ -6,26 +6,26 @@ import junit.framework.TestCase;
 
 import com.edpichler.osgi.bovespa.quote.BovespaProvider;
 import com.edpichler.osgi.bovespa.quote.IBovespaQuoteRetriever;
-import com.edpichler.osgi.bovespa.quote.impl.Quote;
+import com.edpichler.osgi.bovespa.quote.IQuote;
 
 public class BovespaQuotesTest extends TestCase {
 
 	public void testGetQuote() throws Exception {
 		IBovespaQuoteRetriever prov = BovespaProvider.createQuoteRetriever();
-		Quote quote = prov.getQuote("OGXP3");
+		IQuote quote = prov.getQuote("OGXP3");
 		double ultimo = quote.getUltimo();
 		assertTrue(ultimo != 0);
 	}
 
 	public void testGetQuoteInvalid() throws Exception {
 		IBovespaQuoteRetriever prov = BovespaProvider.createQuoteRetriever();
-		Quote quote = prov.getQuote("xxxxx");
+		IQuote quote = prov.getQuote("xxxxx");
 		assertNull(quote);
 	}
 
 	public void testGetQuoteList() throws Exception {
 		IBovespaQuoteRetriever prov = BovespaProvider.createQuoteRetriever();
-		List<Quote> quotes = prov.getQuotes(new String[] { "OGXP3", "VALE5" });
+		List<IQuote> quotes = prov.getQuotes(new String[] { "OGXP3", "VALE5" });
 		for (int i = 0; i < quotes.size(); i++) {
 			double ultimo = quotes.get(i).getUltimo();
 			assertTrue(ultimo != 0);
--- eclipse workspace/OSGi Bovespa Quotes/src/com/edpichler/osgi/bovespa/quote/IBovespaQuoteRetriever.java
+++ eclipse workspace/OSGi Bovespa Quotes/src/com/edpichler/osgi/bovespa/quote/IBovespaQuoteRetriever.java
@@ -2,15 +2,14 @@ package com.edpichler.osgi.bovespa.quote;
 
 import java.util.List;
 
-import com.edpichler.osgi.bovespa.quote.impl.Quote;
-
-
-/**Classe para comunicação com a BMF Bovespa*/
+/** Classe para comunicaÃ§Ã£oo com a BMF Bovespa */
 public interface IBovespaQuoteRetriever {
 	/**
-	 * Retorna a cotação do ativo.
-	 * @throws Exception 
+	 * Retorna a cotaÃ§Ã£o do ativo.
+	 * 
+	 * @throws Exception
 	 * */
-	public List<Quote> getQuotes(String[] codigoAtivo) throws Exception;
-	public Quote getQuote(String codigoAtivo) throws Exception;
+	public List<IQuote> getQuotes(String[] codigoAtivo) throws Exception;
+
+	public IQuote getQuote(String codigoAtivo) throws Exception;
 }
